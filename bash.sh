fae887e4b561e885e1270b4ea734075318527325
Matheus-Nasci <nasci.olv@gmail.com>
2025-08-08 15:29:34 -0300

    lista de arrays dos diretórios e refatorando find

--- bash.sh
+++ bash.sh
@@ -5,11 +5,12 @@ AMBIENCE="dev"
 BATCH_SIZE=10
 ARRAY_FILES=(apis config-map node-port routes)
 
-for FILE in "${ARRAY_FILES[@]}"; do
-  # echo "Procurando no diretório: $FILE no ambiente $AMBIENCE"
+# lista formato yaml
+# "apis config-map node-port routes"
 
-  find . -type d -name "$FILE" | while read path_dir; do
-    find $path_dir -type f -name "*yml" -o -name "*yaml" | while read file; do
+for FILE in "${ARRAY_FILES[@]}"; do
+  find . -type d -name "$FILE" | while read -r path_dir; do
+    find "$path_dir" -type f \( -name "*.yml" -o -name "*.yaml" \) | while read -r file; do
       echo "Arquivo encontrado: $file"
       echo "Aplicando arquivo no ambiente $AMBIENCE"
       oc apply -f "$file" -n "$NAMESPACE-$AMBIENCE" --dry-run=client
